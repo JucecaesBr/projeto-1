3b0a21c1847025f69c52a7d3f4428d2f2b453efb
Julio Cesar <juliocaldasstella@gmail.com>
2023-10-16 15:33:25 -0300

    adicioando arquivos

---
+++ teste1.js
@@ -0,0 +1,12 @@
+for(let cont=1;cont<=10;cont++){
+    console.log(cont)
+}
+
+console.log("ola mundo")
+
+function soma(numero1,numero2){
+    let somador=numero1+numero2
+    console.log("A soma de ",numero1, " e ", numero2, " e igual a ",somador)
+}
+
+soma(5,5)
